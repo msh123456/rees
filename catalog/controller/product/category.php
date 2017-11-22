b6bc896dcf87eb5fd95a5578c047c7522ce58c9a
Meysam <hisfaceacount@gmail.com>
2017-11-22 18:10:36 +0330

    config files for real server added to project

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -219,7 +219,6 @@ class ControllerProductCategory extends Controller
             $results = $this->model_catalog_product->getProducts($filter_data);
 //            $product_total=count($results);
 
-
             foreach ($results as $result) {
                 if ($result['image']) {
                     $image =$server."/image/".$result['image'];// $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
---
+++ config_server.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://rees.ir/reesc2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://rees.ir/reesc2/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/');
+define('DIR_LANGUAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/config/');
+define('DIR_IMAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/image/');
+define('DIR_CACHE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/download/');
+define('DIR_UPLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/upload/');
+define('DIR_MODIFICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/modification/');
+define('DIR_LOGS', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'meysam');
+define('DB_PASSWORD', '00980937');
+define('DB_DATABASE', 'reesc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'dwc_');
