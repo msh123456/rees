407098ab0489ba1695db46de6862d09f7eba1eb7
hisfaceacount@gmail.com <hisfaceacount@gmail.com>
2018-02-05 00:28:36 +0330

    حمله به سایت رفع شد

--- admin/config_admin.php
+++ admin/config_admin.php
@@ -2,26 +2,26 @@
 
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://rees.ir/reesc2/admin/');
-define('HTTP_CATALOG', 'http://rees.ir/reesc2/');
+define('HTTP_SERVER', 'http://rees.ir/admin/');
+define('HTTP_CATALOG', 'http://rees.ir/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://rees.ir/reesc2/admin/');
-define('HTTPS_CATALOG', 'http://rees.ir/reesc2/');
+define('HTTPS_SERVER', 'http://rees.ir/admin/');
+define('HTTPS_CATALOG', 'http://rees.ir/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/admin/');
-define('DIR_SYSTEM', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/admin/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/admin/view/template/');
-define('DIR_CONFIG', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/image/');
-define('DIR_CACHE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/download/');
-define('DIR_UPLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/upload/');
-define('DIR_LOGS', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/logs/');
-define('DIR_MODIFICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/modification/');
-define('DIR_CATALOG', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/');
+define('DIR_APPLICATION', '/var/www/vhosts/rees.ir/httpdocs/admin/');
+define('DIR_SYSTEM', '/var/www/vhosts/rees.ir/httpdocs/system/');
+define('DIR_LANGUAGE', '/var/www/vhosts/rees.ir/httpdocs/admin/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/rees.ir/httpdocs/admin/view/template/');
+define('DIR_CONFIG', '/var/www/vhosts/rees.ir/httpdocs/system/config/');
+define('DIR_IMAGE', '/var/www/vhosts/rees.ir/httpdocs/image/');
+define('DIR_CACHE', '/var/www/vhosts/rees.ir/httpdocs/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/rees.ir/httpdocs/system/download/');
+define('DIR_UPLOAD', '/var/www/vhosts/rees.ir/httpdocs/system/upload/');
+define('DIR_LOGS', '/var/www/vhosts/rees.ir/httpdocs/system/logs/');
+define('DIR_MODIFICATION', '/var/www/vhosts/rees.ir/httpdocs/system/modification/');
+define('DIR_CATALOG', '/var/www/vhosts/rees.ir/httpdocs/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config_server.php
+++ config_server.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://rees.ir/reesc2/');
+define('HTTP_SERVER', 'http://rees.ir/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://rees.ir/reesc2/');
+define('HTTPS_SERVER', 'http://rees.ir/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/');
-define('DIR_SYSTEM', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/');
-define('DIR_LANGUAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/config/');
-define('DIR_IMAGE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/image/');
-define('DIR_CACHE', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/download/');
-define('DIR_UPLOAD', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/upload/');
-define('DIR_MODIFICATION', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/modification/');
-define('DIR_LOGS', '/var/www/vhosts/rees.ir/httpdocs/reesc2/system/logs/');
+define('DIR_APPLICATION', '/var/www/vhosts/rees.ir/httpdocs/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/rees.ir/httpdocs/system/');
+define('DIR_LANGUAGE', '/var/www/vhosts/rees.ir/httpdocs/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/vhosts/rees.ir/httpdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/vhosts/rees.ir/httpdocs/system/config/');
+define('DIR_IMAGE', '/var/www/vhosts/rees.ir/httpdocs/image/');
+define('DIR_CACHE', '/var/www/vhosts/rees.ir/httpdocs/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/vhosts/rees.ir/httpdocs/system/download/');
+define('DIR_UPLOAD', '/var/www/vhosts/rees.ir/httpdocs/system/upload/');
+define('DIR_MODIFICATION', '/var/www/vhosts/rees.ir/httpdocs/system/modification/');
+define('DIR_LOGS', '/var/www/vhosts/rees.ir/httpdocs/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
